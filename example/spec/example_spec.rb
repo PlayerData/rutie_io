804bf6b3f49cfc446d65b7b84b34f1760cdb7fa0
Hayden Ball <hayden@playerdata.com>
2023-08-24 19:25:47 -0400

    test: Check with file as well

--- example/spec/example_spec.rb
+++ example/spec/example_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "tempfile"
+
 require_relative "../rutie_io_example"
 
 RSpec.describe RutieIOExample do
@@ -11,5 +13,19 @@ RSpec.describe RutieIOExample do
 
     string_io_out.rewind
     expect(string_io_out.read).to eq "Hello World"
+  end
+
+  it 'echoes with File' do
+    Tempfile.create do |file_in|
+      file_in.write("Hello World")
+      file_in.rewind
+
+      Tempfile.create do |file_out|
+        RutieIOExample.echo(file_in, file_out)
+
+        file_out.rewind
+        expect(file_out.read).to eq "Hello World"
+      end
+    end
   end
 end
